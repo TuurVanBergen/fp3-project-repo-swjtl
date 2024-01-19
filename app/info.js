581a7f8ceb457468eca393c461665fa25e1eeab6
StasScorobogatskii <114143870+Lordcreeper777@users.noreply.github.com>
2024-01-19 10:15:02 +0100

    text+button+trends

--- app/info.js
+++
@@ -1,4 +0,0 @@
-document.querySelector("#globeTheater").addEventListener("click", function () {
-	document.querySelector("#centuryInformation").style.display = "none";
-	document.querySelector("#info").style.display = "block";
-});
